32b1b252abf11c9b2e64f14752b28596d38540f3
Obed Nuertey <onuertey1997@gmail.com>
2025-04-19 18:39:56 +0000

    feat: set settings privileges
    M  frontend/src/pages/ProductComments.jsx

--- frontend/src/pages/ProductComments.jsx
+++ frontend/src/pages/ProductComments.jsx
@@ -393,7 +393,7 @@ export default function ProductComments() {
                 </div>
 
                 {/* Instagram Tab */}
-                <input type="radio" disabled={creatableAccess} name="my_tabs_2" role="tab" className="tab" aria-label="Instagram" />
+                <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Instagram" />
                 <div role="tabpanel" className="tab-content bg-base-100 border-base-300 max-h-[calc(100vh-20rem)] overflow-y-scroll rounded-box p-2 md:p-6">
                     {comments.instagram.map((comment, id, arr) => (
                         <>

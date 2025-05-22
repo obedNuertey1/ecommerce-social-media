7ed05e0ab4e06944d6a12a9af2b79592294b03df
Obed Nuertey <onuertey1997@gmail.com>
2025-05-22 13:31:31 +0000

    patch: logged passkey details
    files affected: M  frontend/src/pages/AuthPage.jsx

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -113,6 +113,7 @@ export default function AuthPage() {
       // const passkeys = await googleSheet.getSpreadsheetValuesByName2("EcommerceSpreadSheet", passkeySchema.sheetName);
       const passkeys = await fetchPasskeys2(gapi2);
       const passkeyFromSheet = passkeys.find((pk) => pk.passkey == passkey);
+      console.log({passkeyFromSheet});
 
 
       const passkeyExist = Boolean(passkeyFromSheet);

68651cddefff785d9491023a345b46976ffdc174
Obed Nuertey <onuertey1997@gmail.com>
2025-06-05 08:56:33 +0000

    patch: created mechanism that pushes passkey logs to google sheet
    files affected: M  frontend/src/App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -118,7 +118,10 @@ function App() {
     queryFn: () => bulkAddPasskeyLogs(gapi),
     refetchInterval: 1000 * 60 * 4,
     refetchIntervalInBackground: true,
-    enabled: (JSON.parse(localStorage.getItem("passkey_logs"))?.length > 0) && localStorage.getItem("passkey")
+    enabled: ()=>{
+      if((JSON.parse(localStorage.getItem("passkey_logs"))?.length > 0) && localStorage.getItem("passkey")) return true;
+      return false;
+    }
   })
 
   const { playNotification } = useNotifications("orders_notification_sound");

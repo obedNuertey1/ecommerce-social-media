1e8c22d293e40980d7ffe6fe5ca264344297519a
Obed Nuertey <onuertey1997@gmail.com>
2025-06-19 16:33:14 +0000

    fix: added useQuery import to fix errors
    files affected:M  frontend/src/store/usePasskeyLogsStore.js

--- frontend/src/store/usePasskeyLogsStore.js
+++ frontend/src/store/usePasskeyLogsStore.js
@@ -52,6 +52,7 @@ export const usePasskeyLogsStore = create((set, get)=>({
         const {promise, cancel} = cancellableWaiting(1000);
         try{
             const googleSheet = new GoogleSheetsAPI(gapi);
+            console.log("line 55 on usePasskeyLogsStore.js");
             const passkeyLogs = await googleSheet.getSpreadSheetValuesByName2(GOOGLE_SPREADSHEET_NAME, passkeyLogsSchema.sheetName);
             console.log({"works!": passkeyLogs});
             set({passkeyLogs: passkeyLogs, error: null, loading: false});

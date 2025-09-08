e3bd367d51046c3327d965729161abcb9feef7a6
Obed Nuertey <onuertey1997@gmail.com>
2025-09-08 22:13:49 +0000

    ref: debugging AuthPage
    files affected: M  frontend/src/store/useProductStore.js

--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -476,7 +476,7 @@ export const useProductStore = create((set, get) => ({
                 instagramPermalink: postInfo.instagramPermalink,
                 longitude: formData.longitude,
                 latitude: formData.latitude,
-                companyPhone: `["${formData.companyPhone}"]`,
+                companyPhone: JSON.stringify([`${String(formData.companyPhone)}`]),
                 companyName: formData.companyName
 
 
